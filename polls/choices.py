48184d22506bb925ff999ef97486bad31953a380
kendoncowart1 <kendoncowart@gmail.com>
2024-06-28 12:42:39 -0700

    Test

---
+++ polls/choices.py
@@ -0,0 +1,9 @@
+
+
+YEAR_IN_SCHOOL_CHOICES = {
+    "FR": "Freshman",
+    "SO": "Sophomore",
+    "JR": "Junior",
+    "SR": "Senior",
+    "GR": "Graduate",
+}
--- polls/models.py
+++ polls/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+import choices.py
 
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
+    answer = models.CharField(choices=choices.YEAR_IN_SCHOOL_CHOICES)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
